dd88d24ad4ef94d0be79e3e92fc1670d2166e79e
César Esaú Flores Martínez <esauflores2022@gmail.com>
2024-07-18 19:07:21 +0700

    Refactor templates/game -> templates
    Modified the extension .html to .jinja

--- games/games/views.py
+++ games/games/views.py
@@ -15,4 +15,10 @@ navbar = [
 @app.get("/")
 def index(request, name: str = "World"):
     context = {"name": name, "navbarMenu": navbar}
-    return render(request, "games/index.html", context)
+    return render(request, "index.jinja", context)
+
+
+@app.get("/tic-tac-toe")
+def tic_tac_toe(request):
+    context = {"title": "Tic Tac Toe", "navbarMenu": navbar}
+    return render(request, "tic-tac-toe.jinja", context)
